02e96563b1cac52a99cc5481b3147e52d0b69f4b
SHEEL PARIKH <91554427+sheel27@users.noreply.github.com>
2024-02-03 15:11:23 +0530

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -5,29 +5,12 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
 
 Auth::routes();
 
-// Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-// Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
-// Route::get('/profile/edit', [ProfileController::class, 'edit'])->name('profile.edit');
-// Route::put('/profile/update', [ProfileController::class, 'update'])->name('profile.update');
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 Route::middleware(['auth'])->group(function () {
     Route::get('/account', 'UserController@account')->name('account');
     Route::patch('/account', 'UserController@updateAccount')->name('account.update');
